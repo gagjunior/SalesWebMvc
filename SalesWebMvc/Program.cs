9141f88ebdd86c953f0aa115566c31a4f3730855
Gilberto Junior <gagjunior@hotmail.com>
2022-01-30 23:20:44 -0300

    SellersControler

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), 
     Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql")));
 
+builder.Services.AddScoped<SeedingService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -18,6 +20,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    using (var serviceScope = app.Services.CreateScope())
+    {
+        var services = serviceScope.ServiceProvider;
+
+        var myDependency = services.GetRequiredService<SeedingService>();
+        myDependency.Seed();
+    }
 }
 
 app.UseHttpsRedirection();
